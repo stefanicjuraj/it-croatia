e7b24dbcac1bb6af445827ce399ce7d39d5f7fda
stefanicjuraj <jurajstefanic@outlook.com>
2024-03-09 15:25:05 +0100

    feat(components): add Community Table Head component

---
+++ app/src/components/Community/TableHead.tsx
@@ -0,0 +1,20 @@
+export default function TableHead() {
+    return (
+        <thead className="text-lg text-white bg-[#222] border-b border-[#222]">
+            <tr>
+                <th scope="col" className="py-8 px-7">
+                    Community
+                </th>
+                <th scope="col" className="py-8 px-7">
+                    Description
+                </th>
+                <th scope="col" className="py-8 px-7">
+                    Topics
+                </th>
+                <th scope="col" className="py-8 px-7">
+                    Join
+                </th>
+            </tr>
+        </thead>
+    )
+}
