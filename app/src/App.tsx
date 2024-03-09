a8651a0e11cf28182c995e4683786fda8333233d
stefanicjuraj <jurajstefanic@outlook.com>
2024-03-09 14:34:43 +0100

    feat(app): add Home page to App

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,6 +1,14 @@
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import Home from "./pages/Home"
+
 export default function App() {
   return (
     <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Router>
     </>
   )
 }
