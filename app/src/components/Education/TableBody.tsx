c29682be011d866883b9062f932d3cf6100508a6
stefanicjuraj <jurajstefanic@outlook.com>
2024-03-09 15:14:03 +0100

    feat(components): add Education Table Body component

---
+++ app/src/components/Education/TableBody.tsx
@@ -0,0 +1,47 @@
+export const TableBody = ({ education }: { education: Array<object> }) => {
+
+    return (
+        <tbody>
+            {education.map((education, index) => (
+                <tr key={index} className="bg-[#222] border-t border-[#555] text-md">
+                    <td className="text-lg px-7 py-7 whitespace-nowrap">
+                        {(education as { Course: string, Website: string })?.Course}
+                        <br />
+                        <a className="text-sm text-[#999] hover:underline"
+                            href={(education as { Website: string })?.Website}
+                            target="_blank" rel="noopener noreferrer"
+                        >
+                            {(education as { Website: string })?.Website}
+                        </a>
+                    </td>
+                    <td className="px-7 py-7 text-md">
+                        {(education as { Type: string })?.Type}
+                    </td>
+                    <td className="text-xs text-white px-7 py-7">
+                        {(education as { Topic: string[] })?.Topic.map((topic, index) => (
+                            <span key={index} className="px-3 py-2 mr-1 bg-indigo-500 rounded-full">
+                                {topic}
+                            </span>
+                        ))}
+                    </td>
+                    <td className="px-7 py-7 text-md">
+                        <a className="underline"
+                            href={(education as { Organizer: string })?.Organizer}
+                            target="_blank" rel="noopener noreferrer"
+                        >
+                            {(education as { Organizer: string })?.Organizer}
+                        </a>
+                    </td>
+                    <td className="px-10 py-7">
+                        <a className="inline-flex items-center hover:shadow hover:ring-indigo-500 focus:ring-2 focus:outline-none focus:ring-gray-200 rounded-xl"
+                            href={(education as { Enroll: string })?.Enroll}
+                            target="_blank" rel="noopener noreferrer"
+                        >
+                        </a>
+                    </td>
+                </tr>
+            ))}
+        </tbody>
+    )
+
+}
