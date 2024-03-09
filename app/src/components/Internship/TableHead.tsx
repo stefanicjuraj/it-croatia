27981ccb0160d1301ea644d4894dd67e4edba1dc
stefanicjuraj <jurajstefanic@outlook.com>
2024-03-09 15:45:47 +0100

    feat(components): add Internship Table Head component

---
+++ app/src/components/Internship/TableHead.tsx
@@ -0,0 +1,20 @@
+export default function TableHead() {
+    return (
+        <thead className="text-lg text-white bg-[#222] border-b border-[#222]">
+            <tr>
+                <th scope="col" className="py-8 px-7">
+                    Internship
+                </th>
+                <th scope="col" className="py-8 px-7">
+                    Company
+                </th>
+                <th scope="col" className="py-8 px-7">
+                    Department
+                </th>
+                <th scope="col" className="py-8 px-7">
+                    Apply
+                </th>
+            </tr>
+        </thead>
+    )
+}
