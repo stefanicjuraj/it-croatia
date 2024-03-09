f2e5f265b5108bc9d605770ada54a85b49d1a4a6
stefanicjuraj <jurajstefanic@outlook.com>
2024-03-09 15:07:53 +0100

    feat(components): add Conference Table Head component

---
+++ app/src/components/Conference/TableHead.tsx
@@ -0,0 +1,26 @@
+export default function TableHead() {
+    return (
+        <thead className="text-lg text-white bg-[#222] border-b border-[#222]">
+            <tr>
+                <th scope="col" className="py-8 px-7">
+                    Conference
+                </th>
+                <th scope="col" className="py-8 px-7">
+                    Topics
+                </th>
+                <th scope="col" className="py-8 px-7">
+                    Organizer
+                </th>
+                <th scope="col" className="py-8 px-7">
+                    Location
+                </th>
+                <th scope="col" className="py-8 px-7">
+                    Dates
+                </th>
+                <th scope="col" className="py-8 px-7">
+                    Ticket
+                </th>
+            </tr>
+        </thead>
+    )
+}
