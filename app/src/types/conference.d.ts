e3b5a8701a2f75c5835041fccb58e0d129f82af2
stefanicjuraj <jurajstefanic@outlook.com>
2024-03-09 15:02:53 +0100

    feat(types): add Conference interface type

---
+++ app/src/types/conference.d.ts
@@ -0,0 +1,11 @@
+export default interface Conference {
+  Conference: string;
+  Website: string;
+  Topic: string[];
+  Organizer: string;
+  Contact: string;
+  Location: string;
+  startDate: string;
+  endDate: string;
+  Ticket: string;
+}
