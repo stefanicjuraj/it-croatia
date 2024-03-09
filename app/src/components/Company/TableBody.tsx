09dd563915fb830bb9efbb24d5a96e15b18f7edd
stefanicjuraj <jurajstefanic@outlook.com>
2024-03-09 14:56:00 +0100

    feat(components): add Company Table Body component

---
+++ app/src/components/Company/TableBody.tsx
@@ -0,0 +1,43 @@
+export const TableBody = ({ companies }: { companies: Array<object> }) => {
+    return (
+        <tbody>
+            {companies.map((company, index) => (
+                <tr key={index} className="bg-[#222] border-t border-[#444]">
+                    <td className="text-lg px-7 py-7 whitespace-nowrap">
+                        {(company as { Company: string }).Company}
+                        <br />
+                        <a className="text-sm text-[#999] hover:underline"
+                            href={(company as { Website: string })["Website"]}
+                            target="_blank" rel="noopener noreferrer"
+                        >
+                            {(company as { Website: string })["Website"]}
+                        </a>
+                    </td>
+                    <td className="text-sm text-white px-7 py-7">
+                        {(company as { Industry: string[] }).Industry.map((industry, index) => (
+                            <span className="px-3 py-2 mr-1 bg-indigo-500 rounded-full"
+                                key={index}
+                            >
+                                {industry}
+                            </span>
+                        ))}
+                    </td>
+                    <td className="px-10 py-7">
+                        <a className="inline-flex items-center hover:shadow hover:ring-indigo-500 focus:ring-2 focus:outline-none focus:ring-gray-200 rounded-xl"
+                            href={(company as { LinkedIn: string })["LinkedIn"]}
+                            target="_blank" rel="noopener noreferrer"
+                        >
+                        </a>
+                    </td>
+                    <td className="px-8 py-7 text-md">
+                        {(company as { Employees: string }).Employees}
+                    </td>
+                    <td className="px-7 py-7 text-md">
+                        {(company as { Location: string }).Location}
+                    </td>
+                </tr>
+            ))}
+        </tbody>
+    )
+}
+
