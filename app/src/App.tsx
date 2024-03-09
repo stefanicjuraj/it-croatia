e96c5c6d9efeb6d0c20e68e25b69f55724487647
stefanicjuraj <jurajstefanic@outlook.com>
2024-03-09 14:40:56 +0100

    feat(app): add Sidebar component to App

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,10 +1,14 @@
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+// Components
+import Sidebar from './components/Sidebar';
+// Pages
 import Home from "./pages/Home"
 
 export default function App() {
   return (
     <>
       <Router>
+        <Sidebar />
         <Routes>
           <Route path="/" element={<Home />} />
         </Routes>
