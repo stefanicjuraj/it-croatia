2f21e8c45785aef9f0555e5d629e69d01c839ab7
stefanicjuraj <jurajstefanic@outlook.com>
2024-03-09 14:54:53 +0100

    feat(components): add Search component

---
+++ app/src/components/Search.tsx
@@ -0,0 +1,19 @@
+export const Search = ({ search, searchInput, placeholder }: { search: string, searchInput: (event: React.ChangeEvent<HTMLInputElement>) => void, placeholder: string }) => {
+
+    return (
+        <form className="flex items-center relative w-96 mr-4">
+            <div className="relative w-96">
+                <div className="absolute inset-y-0 left-0 flex items-center pl-4 pointer-events-none">
+                </div>
+                <input className="block w-96 p-4 pl-10 text-white bg-[#222] border-[#333] rounded-xl focus:border-indigo-400"
+                    type="text"
+                    value={search}
+                    onChange={searchInput}
+                    placeholder={placeholder}
+                    autoComplete="off"
+                />
+            </div>
+        </form>
+    );
+
+};
