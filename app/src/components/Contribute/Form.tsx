b03be35cb1c522813ebfebf1798bd249ae65d141
stefanicjuraj <jurajstefanic@outlook.com>
2024-03-10 01:08:06 +0100

    feat(components): add Contribute Form component

---
+++ app/src/components/Contribute/Form.tsx
@@ -0,0 +1,80 @@
+import arrow from "/assets/icons/arrow-right.svg";
+import mail from "/assets/icons/mail.svg";
+import user from "/assets/icons/user.svg";
+
+export default function Form() {
+    const accessKey = import.meta.env.VITE_ACCESS_KEY || '';
+
+    return (
+        <div className="max-w-screen-md mx-auto mt-16 mb-4 sm:p-12 p-8 bg-[#222] rounded-xl">
+            <form method="POST" action="https://api.web3forms.com/submit">
+                <input type="hidden" name="access_key" value={accessKey} />
+                <input type="hidden" name="redirect" value=""></input>
+                <input type="hidden" name="from_name" value=""></input>
+                <input type="hidden" name="subject" value="New Contribution"></input>
+                <div className="grid md:grid-cols-2 md:gap-6">
+                    <div className="relative z-0 w-full mt-4 group">
+                        <label htmlFor="name" className="block mb-2 text-md text-white">
+                            Name / Alias
+                        </label>
+                        <div className="flex">
+                            <span className="inline-flex items-center px-3 border border-r-0 border-[#999] rounded-l-md">
+                                <img src={user} className="h-5 w-5" alt="User icon" />
+                            </span>
+                            <input className="rounded-none rounded-r-lg bg-[#333] border border-[#999] text-white focus:ring-[#6875F5] focus:ring-1 focus:border-[#6875F5] block flex-1 min-w-0 w-full text-sm p-3"
+                                type="text"
+                                name="name"
+                                id="name"
+                                maxLength={30}
+                                autoComplete="off"
+                                placeholder="John Doe"
+                            />
+                        </div>
+                        <p className="mt-2 text-sm text-[#555]">
+                            Optional - if you want a mention in contributions.
+                        </p>
+                    </div>
+                    <div className="relative z-0 w-full mt-4 group">
+                        <label htmlFor="email" className="block mb-2 text-md text-white">
+                            Email
+                        </label>
+                        <div className="flex">
+                            <span className="inline-flex items-center px-3 border border-r-0 border-[#999] rounded-l-md">
+                                <img src={mail} className="h-5 w-5" alt="Mail icon" />
+                            </span>
+                            <input className="rounded-none rounded-r-lg bg-[#333] border border-[#999] text-white focus:ring-[#6875F5] focus:ring-1 focus:border-[#6875F5] block flex-1 min-w-0 w-full text-sm p-3"
+                                type="email"
+                                name="email"
+                                id="email"
+                                maxLength={50}
+                                autoComplete="off"
+                                placeholder="john.doe@gmail.com"
+
+                            />
+                        </div>
+                        <p className="mt-2 text-sm text-[#555]">
+                            Optional - if you want a receive a reply.
+                        </p>
+                    </div>
+                </div>
+                <label htmlFor="message" className="block mt-4 mb-2 text-md font-medium text-white">
+                    Your contribution
+                </label>
+                <textarea className="block p-2.5 w-full text-sm text-white bg-[#333] rounded-lg border border-[#999] focus:ring-[#6875F5] focus:border-[#6875F5]"
+                    name="message"
+                    id="message"
+                    rows={5}
+                    maxLength={500}
+                    autoComplete="off"
+                    placeholder="Hi, I would like to submit a company 'Google' to be added."
+                    required
+                >
+                </textarea>
+                <button type="submit" className="mt-8 inline-flex px-5 py-3 mx-auto font-normal text-center text-white bg-indigo-500 rounded-lg text-md hover:bg-indigo-500 focus:ring-1 focus:outline-none focus:ring-indigo-300">
+                    Submit contribution
+                    <img src={arrow} className="w-6 h-6 ms-2" alt="Arrow icon" />
+                </button>
+            </form>
+        </div>
+    )
+}
