19787d82f9dfd8789cd8728c36191e2d9bd382ac
stefanicjuraj <jurajstefanic@outlook.com>
2024-03-12 15:10:18 +0100

    style(components): modify Search form alignment

--- app/src/components/Search.tsx
+++ app/src/components/Search.tsx
@@ -11,12 +11,12 @@ export const Search = ({ placeholder, onSearchChange }: { placeholder: string, o
     };
 
     return (
-        <form className="flex items-center relative w-96 mr-4">
+        <form className="flex items-center relative sm:w-96 w-80 mr-4">
             <div className="relative w-96">
                 <div className="absolute inset-y-0 left-0 flex items-center pl-4 pointer-events-none">
                     <img src={searchicon} className="w-5 h-5" alt="Search icon" />
                 </div>
-                <input className="block w-96 p-4 pl-10 text-white bg-[#222] border-[#333] rounded-xl focus:border-indigo-400 hover:border-indigo-400"
+                <input className="block sm:w-96 w-80 p-4 pl-12 text-white bg-[#222] border-[#333] rounded-xl focus:border-indigo-400 hover:border-indigo-400"
                     type="text"
                     value={search}
                     onChange={handleSearch}
