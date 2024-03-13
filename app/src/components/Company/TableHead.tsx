4c57f0b7a5eb7475ea11515d6d35443dab1c419e
stefanicjuraj <jurajstefanic@outlook.com>
2024-03-13 22:46:20 +0100

    feat(components): add sortEmployees to Company TableHead

--- app/src/components/Company/TableHead.tsx
+++ app/src/components/Company/TableHead.tsx
@@ -1,4 +1,6 @@
-export default function TableHead() {
+import chevron from '/assets/icons/sort.svg';
+
+export default function TableHead({ sortEmployees }: { sortEmployees: () => void }) {
     return (
         <thead className="text-lg text-white bg-[#222] border-b border-[#222]">
             <tr>
@@ -11,8 +13,9 @@ export default function TableHead() {
                 <th scope="col" className="py-8 px-7">
                     Location
                 </th>
-                <th scope="col" className="py-8 px-7">
+                <th scope="col" className="py-8 px-7 flex cursor-pointer" onClick={sortEmployees}>
                     Employees
+                    <img src={chevron} className="h-5 w-5 mt-1 ml-2 transition-transform" />
                 </th>
                 <th scope="col" className="py-8 px-7">
                     LinkedIn
