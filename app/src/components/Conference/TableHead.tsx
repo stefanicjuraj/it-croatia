d5b0a24bef8c665154640a3f4dc436b908322b26
stefanicjuraj <jurajstefanic@outlook.com>
2024-03-13 22:54:21 +0100

    feat(components): add sortDates to Conference TableHead

--- app/src/components/Conference/TableHead.tsx
+++ app/src/components/Conference/TableHead.tsx
@@ -1,4 +1,6 @@
-export default function TableHead() {
+import chevron from '/assets/icons/sort.svg';
+
+export default function TableHead({ sortDates }: { sortDates: () => void }) {
     return (
         <thead className="text-lg text-white bg-[#222] border-b border-[#222]">
             <tr>
@@ -14,8 +16,9 @@ export default function TableHead() {
                 <th scope="col" className="py-8 px-7">
                     Location
                 </th>
-                <th scope="col" className="py-8 px-7">
+                <th scope="col" className="py-8 px-7 flex cursor-pointer" onClick={sortDates}>
                     Dates
+                    <img src={chevron} className="h-5 w-5 mt-1 ml-2 transition-transform" />
                 </th>
                 <th scope="col" className="py-8 px-7">
                     Ticket
