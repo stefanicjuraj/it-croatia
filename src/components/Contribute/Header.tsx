15ce652aa4c994ff6aae5318df2953ca5c70d54a
stefanicjuraj <jurajstefanic@outlook.com>
2024-03-15 01:50:43 +0100

    style(components): remove span animation div from Header

--- src/components/Contribute/Header.tsx
+++ src/components/Contribute/Header.tsx
@@ -3,19 +3,11 @@ import contribute from "/assets/icons/contribute.svg";
 export default function Header() {
     return (
         <header className="max-w-screen-xl mx-auto mt-40 mb-16 sm:p-16 px-8 py-12 bg-[#222] rounded-3xl text-left shadow-md shadow-[#111]">
-            <div className="flex items-center">
+            <div className="flex items-center mb-8">
                 <img src={contribute} className="sm:w-12 w-9 mr-3" alt="Community icon" />
                 <h1 className="text-4xl font-bold text-indigo-300 sm:text-6xl">
                     Contribute
                 </h1>
-            </div>
-            <div className="flex my-4">
-                <p className="inline-flex items-center text-white">
-                    <span className="flex items-center">
-                        <span className="flex flex-shrink-0 w-2 h-2 mr-2 bg-indigo-400 rounded-full animate-pulse"></span>
-                    </span>
-                    Contributions are open
-                </p>
             </div>
             <p className="w-full text-md sm:text-lg text-white sm:w-3/4">
                 Thank you for your interest in contributing! We value your input and encourage you to share your expertise by suggesting new additions, updates, and providing valuable feedback.
--- src/components/Internship/Header.tsx
+++ src/components/Internship/Header.tsx
@@ -3,19 +3,11 @@ import internship from "/assets/icons/internship.svg";
 export default function Header() {
     return (
         <header className="max-w-screen-xl mx-auto mt-40 mb-16 sm:p-16 px-8 py-12 bg-[#222] rounded-3xl text-left shadow-md shadow-[#111]">
-            <div className="flex items-center">
+            <div className="flex items-center mb-8">
                 <img src={internship} className="sm:w-12 w-9 mr-3" alt="Internship icon" />
                 <h1 className="text-4xl font-bold text-indigo-300 sm:text-6xl">
                     Internships
                 </h1>
-            </div>
-            <div className="flex my-4">
-                <p className="inline-flex items-center text-white">
-                    <span className="flex items-center">
-                        <span className="flex flex-shrink-0 w-2 h-2 mr-2 bg-indigo-400 rounded-full animate-pulse"></span>
-                    </span>
-                    In development
-                </p>
             </div>
             <p className="w-full text-md sm:text-lg text-white sm:w-3/4">
                 A collection of IT <span className="bg-[#333] px-1 py-0.3 rounded-lg">internships</span> {" "} and {" "}
