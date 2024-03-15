b9dd2fcbba271614654581c2da815f5350bff724
stefanicjuraj <jurajstefanic@outlook.com>
2024-03-15 01:44:21 +0100

    style(components): add contribute icon to Contribute Header

--- src/components/Contribute/Header.tsx
+++ src/components/Contribute/Header.tsx
@@ -1,8 +1,10 @@
+import contribute from "/assets/icons/contribute.svg";
 
 export default function Header() {
     return (
         <header className="max-w-screen-xl mx-auto mt-40 mb-16 sm:p-16 px-8 py-12 bg-[#222] rounded-3xl text-left shadow-md shadow-[#111]">
             <div className="flex items-center">
+                <img src={contribute} className="sm:w-12 w-9 mr-3" alt="Community icon" />
                 <h1 className="text-4xl font-bold text-indigo-300 sm:text-6xl">
                     Contribute
                 </h1>
