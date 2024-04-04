ea3013a3e259144012f9829702f40f3d55f8f010
stefanicjuraj <jurajstefanic@outlook.com>
2024-04-04 23:08:03 +0200

    feat(components): add Footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,18 @@
+// Icons
+import github from '/assets/icons/github.svg';
+import linkedin from '/assets/icons/linkedin-light.svg';
+
+export default function Footer() {
+    return (
+        <footer className="text-center mx-auto rounded-lg shadow mb-24">
+            <div className="inline-flex">
+                <a href="https://github.com/stefanicjuraj/it-croatia" target="_blank" rel="noopener noreferrer">
+                    <img src={github} alt="GitHub icon" className="w-8 h-8 mx-2" />
+                </a>
+                <a href="https://www.linkedin.com/in/jurajstefanic/" target="_blank" rel="noopener noreferrer">
+                    <img src={linkedin} alt="LinkedIn icon" className="w-8 h-8 mx-2" />
+                </a>
+            </div>
+        </footer>
+    )
+}
