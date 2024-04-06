de70acc5822f4a408acca34d3307cb48fc0743d4
stefanicjuraj <jurajstefanic@outlook.com>
2024-04-06 14:33:49 +0200

    feat(pages): add Certificate Organizer filter to Certificates page

--- src/pages/Certificates.tsx
+++ src/pages/Certificates.tsx
@@ -1,22 +1,40 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 // Hooks
 import { useCertificate } from '../hooks/useCertificate';
 // Components
 import Loading from '../components/Loading';
 import Header from '../components/Certificate/Header';
 import { Search } from '../components/Search';
+import { Organizer } from '../components/Education/Organizer';
 import TableHead from '../components/Certificate/TableHead';
 import { TableBody } from '../components/Certificate/TableBody';
 
 export default function Certificates() {
     const { certificates, loading, error } = useCertificate();
     const [certificateSearch, setCertificateSearch] = useState("");
+    const [selectOrganizer, setselectOrganizer] = useState<string[]>([]);
+    const [organizer, setOrganizer] = useState<string[]>([]); ([]);
+
+    useEffect(() => {
+        const organizer = [...new Set(certificates.map(certificates => certificates.Organizer))]
+            .sort((a, b) => a.toLowerCase().localeCompare(b.toLowerCase()));
+        setOrganizer(organizer);
+    }, [certificates]);
 
     const searchCertificate = certificates.filter((certificates) => {
         const certificateName = certificates.Certificate.toLowerCase();
+        const certificateOrganizer = selectOrganizer.length === 0 || selectOrganizer.includes(certificates.Organizer);
         const search = certificateSearch.toLowerCase();
-        return certificateName.includes(search);
+        return certificateName.includes(search) && certificateOrganizer;
     });
+
+    const handleOrganizerCheckboxInput = (organizer: string) => {
+        setselectOrganizer(prevOrganizer =>
+            prevOrganizer.includes(organizer)
+                ? prevOrganizer.filter(l => l !== organizer)
+                : [...prevOrganizer, organizer]
+        );
+    };
 
 
     if (error) {
@@ -41,6 +59,11 @@ export default function Certificates() {
                                     {searchCertificate.length} results
                                 </p>
                             </div>
+                            <Organizer
+                                organizer={organizer}
+                                selectOrganizer={selectOrganizer}
+                                checkboxInput={handleOrganizerCheckboxInput}
+                            />
                         </div>
                     </section>
                     <section className="px-4 mx-auto mb-40">
