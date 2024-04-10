589e32339c116c1ec3195cb78bb364c0831be218
stefanicjuraj <jurajstefanic@outlook.com>
2024-04-10 20:45:56 +0200

    feat(components): add Reddit & Glassdoor to Company TableHead

--- src/components/Company/TableHead.tsx
+++ src/components/Company/TableHead.tsx
@@ -7,18 +7,24 @@ export default function TableHead({ sortEmployees }: { sortEmployees: () => void
                 <th scope="col" className="py-8 px-7">
                     Company
                 </th>
-                <th scope="col" className="py-8 px-7">
+                <th scope="col" className="py-8 px-5">
                     Industry
                 </th>
                 <th scope="col" className="py-8 px-7">
                     Location
                 </th>
-                <th scope="col" className="flex py-8 cursor-pointer px-7" onClick={sortEmployees}>
+                <th scope="col" className="flex py-8 cursor-pointer px-5" onClick={sortEmployees}>
                     Employees
                     <img src={chevron} className="w-5 h-5 mt-1 ml-2 transition-transform" />
                 </th>
-                <th scope="col" className="py-8 px-7">
+                <th scope="col" className="py-8 px-6">
                     LinkedIn
+                </th>
+                <th scope="col" className="py-8 px-3">
+                    Glassdoor
+                </th>
+                <th scope="col" className="py-8 px-7">
+                    Reddit
                 </th>
             </tr>
         </thead>
