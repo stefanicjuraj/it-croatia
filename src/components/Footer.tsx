341ea71bd443a1e6b988569cdaeefca813dc3555
stefanicjuraj <jurajstefanic@outlook.com>
2024-04-10 20:44:37 +0200

    style(components): remove Footer shadow styling

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ import linkedin from '/assets/icons/linkedin-light.svg';
 
 export default function Footer() {
     return (
-        <footer className="text-center mx-auto rounded-lg shadow mb-24">
+        <footer className="text-center mx-auto rounded-lg mb-24">
             <div className="inline-flex">
                 <a href="https://github.com/stefanicjuraj/it-croatia" target="_blank" rel="noopener noreferrer">
                     <img src={github} alt="GitHub icon" className="w-8 h-8 mx-2" />
