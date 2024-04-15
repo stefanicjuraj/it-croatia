2c7390fb6ac659713619a5ce2c68f03a27138f58
stefanicjuraj <jurajstefanic@outlook.com>
2024-04-16 01:43:44 +0200

    refactor(pages): format heading

--- src/pages/Data.tsx
+++ src/pages/Data.tsx
@@ -18,10 +18,11 @@ export default function Data() {
             </header>
 
             <div className="max-w-screen-xl mx-auto mt-16 mb-16 sm:p-16 px-8 py-12 bg-[#222] rounded-3xl text-left shadow-md shadow-[#111]">
-                <h2 className="text-white sm:text-3xl text-xl">Companies per city in IT Croatia</h2>
+                <h2 className="text-white sm:text-3xl text-xl">
+                    Companies per city in IT Croatia
+                </h2>
             </div>
             <div className="text-center mx-auto mt-32">
-                <h1>test</h1>
                 <iframe title="IT companies per city in Croatia" aria-label="Map" id="datawrapper-chart-RvbE4" src="https://datawrapper.dwcdn.net/RvbE4/5/" scrolling="no" style={{ padding: "8px", marginLeft: "auto", marginRight: "auto", borderRadius: "16px" }} frameBorder="0" width="600" height="732" data-external="1"></iframe>
             </div>
         </>
