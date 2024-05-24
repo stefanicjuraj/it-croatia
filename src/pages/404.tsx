77ef1f002f60bee0f3184f73c9e3702d1d967a80
stefanicjuraj <jurajstefanic@outlook.com>
2024-05-24 20:46:23 +0200

    style(pages): add backgroud styling & modify paths to pages

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -9,7 +9,7 @@ export default function Header() {
     const style = themeClasses(theme);
 
     return (
-        <header className={`max-w-screen-xl mx-auto mb-16 sm:p-16 px-8 py-12 ${style.backgroundHeader} rounded-3xl text-left ${style.shadowHeader}`}>
+        <header className={`max-w-screen-xl mt-40 mx-auto mb-16 sm:p-16 px-8 py-12 ${style.backgroundHeader} rounded-3xl text-left ${style.shadowHeader}`}>
             <div className="flex items-center mb-8">
                 <img src={fourofour} className="mr-3 sm:w-12 w-9" alt="Company icon" />
                 <h1 className={`text-4xl font-bold ${style.headingH1} sm:text-6xl`}>
@@ -18,38 +18,35 @@ export default function Header() {
             </div>
             <p className={`w-full ${style.text} text-md sm:text-lg sm:w-3/4`}>
                 Oops! Looks like you got lost. Try another page: {" "}
-                <span className="bg-[#333] px-1 py-0.3 rounded-lg underline">
+                <span className={`bg-[#333] ${style.backgroundHeaderText} px-1 py-0.3 rounded-lg underline`}>
                     <Link to="/companies">companies</Link>
                 </span>,{" "}
-                <span className="bg-[#333] px-1 py-0.3 rounded-lg underline">
-                    <Link to="/companies">education</Link>
+                <span className={`bg-[#333] ${style.backgroundHeaderText} px-1 py-0.3 rounded-lg underline`}>
+                    <Link to="/conferences">conferences</Link>
                 </span>,{" "}
-                <span className="bg-[#333] px-1 py-0.3 rounded-lg underline">
-                    <Link to="/companies">certificates</Link>
+                <span className={`bg-[#333] ${style.backgroundHeaderText} px-1 py-0.3 rounded-lg underline`}>
+                    <Link to="/education">education</Link>
                 </span>,{" "}
-                <span className="bg-[#333] px-1 py-0.3 rounded-lg underline">
-                    <Link to="/companies">communities</Link>
+                <span className={`bg-[#333] ${style.backgroundHeaderText} px-1 py-0.3 rounded-lg underline`}>
+                    <Link to="/certificates">certificates</Link>
                 </span>,{" "}
-                <span className="bg-[#333] px-1 py-0.3 rounded-lg underline">
-                    <Link to="/companies">places to work</Link>
+                <span className={`bg-[#333] ${style.backgroundHeaderText} px-1 py-0.3 rounded-lg underline`}>
+                    <Link to="/communities">communities</Link>
                 </span>,{" "}
-                <span className="bg-[#333] px-1 py-0.3 rounded-lg underline">
-                    <Link to="/companies">podcasts</Link>
+                <span className={`bg-[#333] ${style.backgroundHeaderText} px-1 py-0.3 rounded-lg underline`}>
+                    <Link to="/places-to-work">places to work</Link>
                 </span>,{" "}
-                <span className="bg-[#333] px-1 py-0.3 rounded-lg underline">
-                    <Link to="/companies">internships</Link>
+                <span className={`bg-[#333] ${style.backgroundHeaderText} px-1 py-0.3 rounded-lg underline`}>
+                    <Link to="/podcasts">podcasts</Link>
                 </span>,{" "}
-                <span className="bg-[#333] px-1 py-0.3 rounded-lg underline">
-                    <Link to="/companies">blog</Link>
+                <span className={`bg-[#333] ${style.backgroundHeaderText} px-1 py-0.3 rounded-lg underline`}>
+                    <Link to="/internships">internships</Link>
                 </span>,{" "}
-                <span className="bg-[#333] px-1 py-0.3 rounded-lg underline">
-                    <Link to="/companies">contribute</Link>
-                </span>,{" "}
-                <span className="bg-[#333] px-1 py-0.3 rounded-lg underline">
-                    <Link to="/companies">companies</Link>
+                <span className={`bg-[#333] ${style.backgroundHeaderText} px-1 py-0.3 rounded-lg underline`}>
+                    <Link to="/contribute">contributions</Link>
                 </span>{" "} or go to the {" "}
-                <span className="bg-[#333] px-1 py-0.3 rounded-lg underline">
-                    <Link to="/companies">home page</Link>
+                <span className={`bg-[#333] ${style.backgroundHeaderText} px-1 py-0.3 rounded-lg underline`}>
+                    <Link to="/">home page</Link>
                 </span>.
             </p>
         </header>
