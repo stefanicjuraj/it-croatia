29ccf94736a99a9410057186733d6d92ba5f29a0
stefanicjuraj <jurajstefanic@outlook.com>
2024-11-09 01:47:34 +0100

    chore(style): remove x-axis margin & modify font size

--- src/components/Company/Header.tsx
+++ src/components/Company/Header.tsx
@@ -8,12 +8,12 @@ export default function Header() {
     const style = themeClasses(theme);
 
     return (
-        <header className={`max-w-screen-xl mx-auto mb-16 sm:p-16 px-8 py-12 ${style.backgroundHeader} rounded-3xl text-left ${style.shadowHeader} mx-3`}>
+        <header className={`max-w-screen-xl mx-auto mb-16 sm:p-16 px-8 py-12 ${style.backgroundHeader} rounded-3xl text-left ${style.shadowHeader}`}>
             <div className="flex items-center mb-8">
                 <div className={`p-2 rounded-lg ${style.backgroundBody} mr-3`}>
                     <img src={company} className="w-9 w-9" alt="Company icon" />
                 </div>
-                <h1 className={`text-4xl font-black ${style.headingH1} sm:text-6xl`}>
+                <h1 className={`text-4xl font-bold ${style.headingH1} sm:text-6xl`}>
                     Companies
                 </h1>
             </div>
