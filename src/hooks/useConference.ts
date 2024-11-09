6f2c44a91690e02f13572c5394386831434222c8
stefanicjuraj <jurajstefanic@outlook.com>
2024-11-09 01:45:54 +0100

    chore: remove text from daysLeft

--- src/hooks/useConference.ts
+++ src/hooks/useConference.ts
@@ -41,7 +41,7 @@ export const useConference = () => {
     const difference = start.getTime() - now.getTime();
     const daysLeft = Math.ceil(difference / (1000 * 3600 * 24));
   
-    return daysLeft > 0 ? `in ${daysLeft} days` : "";
+    return daysLeft > 0 ? `${daysLeft} days` : "";
   };
 
   return {
