1b1f5f2a8ebf0f82d233d564628f67202d286af8
stefanicjuraj <jurajstefanic@outlook.com>
2024-12-25 21:54:06 +0100

    refactor: reduce icon size for github and linkedin image links

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -8,10 +8,10 @@ export default function Footer() {
             <hr className="border-[#333] mb-16" />
             <div className="inline-flex">
                 <a href="https://github.com/stefanicjuraj/it-croatia" className="mr-2 py-2 px-2 hover:bg-[#333] bg-[#333] rounded-full" target="_blank" rel="noopener noreferrer">
-                    <img src={github} alt="GitHub icon" className="w-7 h-7" />
+                    <img src={github} alt="GitHub icon" className="w-5 h-5" />
                 </a>
                 <a href="https://www.linkedin.com/posts/jurajstefanic_from-the-croit-community-on-reddit-activity-7174729313961795584-hFgt?utm_source=share&utm_medium=member_desktop" className="ml-2 py-2 px-2 hover:bg-[#333] bg-[#333] rounded-full" target="_blank" rel="noopener noreferrer">
-                    <img src={linkedin} alt="LinkedIn icon" className="w-7 h-7" />
+                    <img src={linkedin} alt="LinkedIn icon" className="w-5 h-5" />
                 </a>
             </div>
         </footer>
